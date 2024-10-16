364d5aa868a2dd66e2875ee000694b501cda4b14
Gen <149046501+23jy0115@users.noreply.github.com>
2024-10-16 15:28:07 +0900

    日本電子専門学校の１行を追加

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 Public class Hello{
   public static void main(String[] args){
-    System.out.println("Hello");
+    print();
+    System.out.println("日本電子専門学校");
+  }
+
+  static void print(){
+    for(int i= 0; i < 2; i++){
+      System.out.println("Hello World");
+    } 
   }
 }
