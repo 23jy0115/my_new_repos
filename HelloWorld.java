7c509b6bedf7de5d4d9bd81e7976194d230d4eba
'23jy0115' <'23jy0115@jec.ac.jp'>
2024-10-23 16:08:06 +0900

    日本電子専門学校をもっと表示

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,18 @@
 Public class Hello{
   public static void main(String[] args){
     print();
-    System.out.println("日本電子専門学校");
+    printSchool();
   }
 
   static void print(){
     for(int i= 0; i < 2; i++){
       System.out.println("Hello World");
+    } 
+  }
+  
+  static void printSchool(){
+    for(int i= 0; i < 5; i++){
+      System.out.println("日本電子専門学校");
     } 
   }
 }
